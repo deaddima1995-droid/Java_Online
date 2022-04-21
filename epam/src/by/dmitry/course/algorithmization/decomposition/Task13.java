def78bcdf31e3746d9d6dbe4ec04f6d050bf672e
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-04-21 19:11:43 +0300

    Algorithmization

--- epam/src/by/dmitry/course/algorithmization/decomposition/Task13.java
+++ epam/src/by/dmitry/course/algorithmization/decomposition/Task13.java
@@ -3,6 +3,40 @@ package by.dmitry.course.algorithmization.decomposition;
 public class Task13 {
 
     public static void main(String[] args) {
+        printPairTwinNumbersFromRange(100);
+    }
 
+    public static int[] getPairFromIteration(int iteration) {
+        int[] out = new int[2];
+        out[0] = 6 * iteration - 1;
+        out[1] = out[0] + 2;
+        return out;
+    }
+
+    public static boolean isPrimeNumber(int number) {
+        if (number == 1) {
+            return false;
+        }
+        int count = 2;
+        while (count * count <= number) {
+            if (number % count == 0) {
+                return false;
+            }
+            count += 1;
+        }
+        return true;
+    }
+
+    public static void printPairTwinNumbersFromRange(int range) {
+        int[] pair;
+        if (range <= 3) {
+            System.out.println("\t" + 3 + " " + 5);
+        }
+        for (int i = 1; i <= 2 * range; i++) {
+            pair = getPairFromIteration(i);
+            if (isPrimeNumber(pair[0]) && pair[0] >= range && pair[1] <= 2 * range) {
+                System.out.print("\t" + pair[0] + " " + pair[1]);
+            }
+        }
     }
 }
--- epam/src/by/dmitry/course/algorithmization/decomposition/Task14.java
+++ epam/src/by/dmitry/course/algorithmization/decomposition/Task14.java
@@ -3,6 +3,43 @@ package by.dmitry.course.algorithmization.decomposition;
 public class Task14 {
 
     public static void main(String[] args) {
-        // 1 -9 10-19 100-  153  135 351 -9999 10000-
+        long start = System.currentTimeMillis();
+        printNumbersArmstrongFromRange(100000000);
+        long finish = System.currentTimeMillis() - start;
+        System.out.printf("Время выполнения : %.2f сек", (double)finish / 1000);
+    }
+
+    public static int[][] generateMatrixDegreeNumbers(int range) {
+        int[][] out = new int[10][Task10.getCountNumbers(range)];
+        for (int i = 0; i < out.length; i++) {
+            for (int j = 0; j < out[i].length; j++) {
+                out[i][j] = (int) Math.pow(i, j);
+            }
+        }
+        return out;
+    }
+
+    public static void printNumbersArmstrongFromRange(int range) {
+        int[][] digits = generateMatrixDegreeNumbers(range);
+        for (int i = 1; i < range; i++) {
+            if (isNumberOfArmstrong(i, digits)) {
+                System.out.println("\t" + i);
+            }
+        }
+    }
+    // 153 генерируем простое число например 135 считаем 1^3 + 3^3 + 5^3 и сравниваем с остальными такими же числами 135 153 315 351 513 531
+    // 1634 = 1346 1364 1436 1463 16
+
+    public static boolean isNumberOfArmstrong(int number, int[][] digits) {
+        int digit = Task10.getCountNumbers(number);
+        int out = number;
+        int sum = 0;
+        while (number > 0) {
+            int temp = number % 10;
+            //sum += Math.pow(temp, digit);
+            sum += digits[temp][digit];
+            number /= 10;
+        }
+        return sum == out;
     }
 }
