67da670bf6416df0917a39ed82be1d7be7c1d027
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-04-26 23:44:49 +0300

    String

--- epam/src/by/dmitry/course/strings/array/Task2.java
+++ epam/src/by/dmitry/course/strings/array/Task2.java
@@ -1,4 +1,24 @@
 package by.dmitry.course.strings.array;
 
+import java.util.Arrays;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+       char[] text = "textWorld".toCharArray();
+       text = replaceWordInCharArray(text,4,"Golllllll".toCharArray());
+       Task1.printCharArray(text);
+    }
+
+    public static char[] replaceWordInCharArray(char[] chars, int lastIndex, char[] word) {
+        char[] out = Arrays.copyOf(Arrays.copyOfRange(chars,0,lastIndex), lastIndex + word.length);
+        for (int i = lastIndex, y = 0; y < word.length; i++, y++) {
+            out[i] = word[y];
+        }
+        return out;
+    }
+
+    public static void replaceAllWordInCharArray(char[] chars,char[] oldWord, char[] newWord) {
+
+    }
 }
